f0e5cd1f1781812faec857b40192b31bd4b405f8
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-05-13 18:44:03 +0700

    duck animation

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,20 +6,25 @@ public class Player : MonoBehaviour
     private float jumpForce = 15f; // Control the jump force of the player
     private Rigidbody2D rb; // Reference to the Rigidbody2D component
     private bool isGrounded; // Check if the player is on the ground
-
     [SerializeField]
     private Transform groundCheck; // Reference to the ground check object
-
     [SerializeField]
     private float groundCheckRadius = 0.2f; // Radius of the ground check sphere
-
     [SerializeField]
     private LayerMask groundLayer; // Layer mask to specify which layers are considered ground
+    private Animator animator; // Reference to the Animator component
+    [SerializeField]
+    private BoxCollider2D normalCollider; // Reference to the BoxCollider2D component
+    [SerializeField]
+    private CapsuleCollider2D duckCollider; // Reference to the CapsuleCollider2D component
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the player
+        animator = GetComponent<Animator>(); // Get the Animator component attached to the player
+        normalCollider.enabled = true; // Enable the normal collider
+        duckCollider.enabled = false; // Disable the duck collider
     }
 
     // Update is called once per frame
@@ -27,6 +32,7 @@ public class Player : MonoBehaviour
     {
         isGrounded = CheckIfGrounded(); // Check if the player is grounded
         HandleJump(); // Handle player jump input
+        HandleDuck(); // Handle player duck input
     }
 
     private bool CheckIfGrounded()
@@ -41,12 +47,28 @@ public class Player : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
-    public void HandleJump()
+    private void HandleJump()
     {
         // Check if the player is grounded before allowing a jump
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = Vector2.up * jumpForce; // Apply an upward force to the player
+        }
+    }
+
+    private void HandleDuck()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+           normalCollider.enabled = false; // Disable the normal collider
+            duckCollider.enabled = true; // Enable the duck collider
+            animator.SetBool("isDuck", true); // Set the parameter in the animator to true
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            normalCollider.enabled = true; // Enable the normal collider
+            duckCollider.enabled = false; // Disable the duck collider
+            animator.SetBool("isDuck", false); // Set the parameter in the animator to false
         }
     }
 }
