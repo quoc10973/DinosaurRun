29263694c8ffb9a0c1b88ce433c0e21c3aebdade
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-05-16 15:45:02 +0700

    background

---
+++ Assets/Script/Parallax.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Material material; // Reference to the material of the GameObject
+    [SerializeField]
+    private float parallaxFactor = 0.01f; // Control the speed of the parallax effect
+    private float offset; // Offset value for the parallax effect
+    [SerializeField]
+    private float gameSpeed = 5f; // Speed of the game
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        material = GetComponent<Renderer>().material; // Get the material of the GameObject
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParalaxScroll(); // Call the parallax scroll method
+    }
+
+    private void ParalaxScroll()
+    {
+        float speed = gameSpeed * parallaxFactor; // Calculate the speed of the parallax effect
+        offset += speed * Time.deltaTime; // Update the offset value based on the speed and time
+        material.SetTextureOffset("_MainTex", Vector2.right * offset); // Set the texture offset of the material
+    }
+}
